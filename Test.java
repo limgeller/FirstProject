b1ab0c1a7681df48fea6610d66eafebf603280fe
geller.lim <thflovee@nate.com>
2018-08-16 13:06:20 +0900

    branch2 추가

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ class Test{
 	public static void main(STring [] args){
 		System.out.println("Hello git");
 		System.out.println("hi git");
-		System.out.println("branch1");
+		System.out.println("branch2");
 	}
 }
